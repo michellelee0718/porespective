3eb02e93702fce9975cc3dad05030e81f1e53b3a
Melody Yu <wentingyu03@g.ucla.edu>
2025-02-19 21:37:39 -0800

    feat: implement day/night mode

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from "./pages/Profile";
+import { ThemeProvider } from './context/ThemeContext';
+import { useTheme } from './context/ThemeContext';
 import { useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from "./firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
+import ThemeToggle from './components/ThemeToggle';
 
-function App() {
+function AppContent() {
+  const { isDarkMode } = useTheme();
   const [isAuth, setIsAuth] = useState(false);
   const [user] = useAuthState(auth);
 
@@ -28,33 +32,47 @@ function App() {
   };
 
   return (
-  <Router>
-    <nav>
-      <span>
-        <Link to="/"> home </Link>
-      </span>
+    <div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <Router>
+        <nav>
+          <div className="nav-content">
+            <div className="nav-left">
+              <Link to="/"> home </Link>
+            </div>
+            <div className="right-nav">
+              <span className='user'>
+                {!user ? (
+                  <Link to="/login"> login </Link>
+                ) : (
+                  <div className="user-menu">
+                    <div className="user-top">
+                      <div>{user?.displayName}</div>
+                      <img src={user?.photoURL || ""} height="30px" width="30px" alt=""/>
+                      <Link to="/profile"> profile </Link>
+                      <Link onClick={signUserOut}> log out </Link>
+                    </div>
+                    <ThemeToggle />
+                  </div>
+                )}
+              </span>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
-      <div className="right-nav">
-        <span className='user'>
-          {!user ? (
-            <Link to="/login"> login </Link>
-          ) : (
-              <>
-                <div>{user?.displayName}</div>
-                <img src={user?.photoURL || ""} height="30px" width="30px" alt=""/>
-                <Link to="/profile"> profile </Link>
-                <Link onClick={signUserOut}> log out </Link>
-              </>
-            )}
-        </span>
-      </div>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </Router>
+function App() {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 

950c8812aa7778c89312d954ebcb2fce5e2d4276
Yuqi Huang <yuki19@g.ucla.edu>
2025-03-01 23:08:55 -0800

    implemented AI recommendation streaming feature

---
+++ backend/callback.py
@@ -0,0 +1,9 @@
+from queue import Queue
+from langchain.callbacks.base import BaseCallbackHandler
+
+class StreamingCallbackHandler(BaseCallbackHandler):
+    def __init__(self, queue: Queue):
+        self.queue = queue
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.queue.put(token)
